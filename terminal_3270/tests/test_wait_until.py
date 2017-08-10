cdc53120a3930deac41dd29c7a642e19e1314704
Droffner, Andrew (ad718x) <ad718x@us.att.com>
2017-08-10 14:26:05 -0400

    [INC-1685] Create a WaitUntil class to poll  for a change; in our case, we want to see that the 3270 screen changes by reading a string on it.

---
+++ terminal_3270/tests/test_wait_until.py
@@ -0,0 +1,60 @@
+from unittest import TestCase, mock
+
+from terminal_3270.wait_until import WaitUntil
+
+FOUND_ARGS = (1, 2, 'ok')
+FOUND_KWARG = {'eat': 'shorts', 'qwerty': 12}
+
+
+def generate_false(max_count=10000):
+    for k in range(max_count):
+        yield False
+
+
+class TestWaitUntil(TestCase):
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_wait_until_found(self):
+
+        mock_event = mock.MagicMock()
+        mock_event.found_routine.side_effect = [False, False, True]
+
+        time_limit = 0.725
+
+        wu_runner = WaitUntil(time_limit, mock_event.found_routine, *FOUND_ARGS, **FOUND_KWARG)
+        wu_runner.poll()
+
+        mock_event.found_routine.mock_calls = [
+            mock.call(*FOUND_ARGS, **FOUND_KWARG),  # False - continue
+            mock.call(*FOUND_ARGS, **FOUND_KWARG),  # False - continue
+            mock.call(*FOUND_ARGS, **FOUND_KWARG),  # True - stop!
+        ]
+        self.assertTrue(wu_runner.elapsed < time_limit)
+
+    def test_wait_until_time_limit(self):
+
+        mock_event = mock.MagicMock()
+        mock_event.found_routine.side_effect = generate_false()
+
+        time_limit = 0.025  # 25 ms should finish before our side_effect!
+
+        wu_runner = WaitUntil(time_limit, mock_event.found_routine, *FOUND_ARGS, **FOUND_KWARG)
+        wu_runner.poll()
+
+        mock_event.found_routine.mock_calls[-1] = mock.call(*FOUND_ARGS, **FOUND_KWARG)  # False - end on time limit
+        self.assertTrue(wu_runner.elapsed >= time_limit)
+
+    def test_wait_until_bad_found_function(self):
+
+        with self.assertRaisesRegexp(TypeError, r'^"found_callable"'):
+            WaitUntil(0.1, "not_a_callable")
+
+    def test_wait_until_bad_found_time_limit(self):
+
+        with self.assertRaisesRegexp(ValueError, r'^"time_limit"'):
+            WaitUntil(-120.1, int, *('38'))
---
+++ terminal_3270/wait_until.py
@@ -0,0 +1,78 @@
+from timeit import default_timer as timer
+
+
+class WaitUntil(object):
+    """ Wait Until an Event Occurs or Time Limit.
+
+    The WaitUntil object is an easy way to poll until a change is met.
+    Eventually, the `time_limit` will be reached to indicate failure.
+
+        found_callable(*args, **kwargs) returns True when it meets the condition.
+    """
+
+    def __init__(self, time_limit, found_callable, *args, **kwargs):
+        """ Create WaitUntil poller.
+
+        :param float time_limit: maximum time limit in fractional seconds
+        :param callable found_callable: a callable that returns True when it finds the state
+        :param tuple args: found_callable args parameters
+        :param dict kwargs: found_callable kwargs parameters
+        :raises: TypeError or ValueError for invalid found_callable or time_limit, respectively
+        """
+
+        if not callable(found_callable):
+            raise TypeError('"found_callable" must be a bool callable function')
+
+        if time_limit <= 0.0:
+            ValueError('"time_limit" must be positive number, e.g. 0.725 seconds')
+
+        self.time_limit = time_limit
+        self.found_callable = found_callable
+        self.call_args = args
+        self.call_kwargs = kwargs
+
+        self.start_t = 0.0
+        self.end_t = 0.0
+
+    def poll(self):
+        """ Poll Until Found.
+
+        Poll this WaitUntil object until the found state or time limit.
+        """
+
+        self.start_t = timer()
+
+        while not self.found_callable(*self.call_args, **self.call_kwargs):
+            curr_t = timer()
+            if (curr_t - self.start_t) >= self.time_limit:
+                break
+
+        self.end_t = timer()
+
+    @property
+    def elapsed(self):
+        return (self.end_t - self.start_t)
+
+
+if __name__ == '__main__':  # pragma: no cover
+
+    class Clicker(object):
+        """ Clicker Example
+
+        Run a counting clicker until it reaches a a number.
+        """
+
+        def __init__(self):
+            self.counter = 0
+
+        def click_button(self, *args, **kwargs):
+            self.counter += 1
+            print('Click! counter=%d call(args=%r, kwargs=%r)' % (self.counter, args, kwargs))
+            return (self.counter > 3000 * 100)
+
+    clicker = Clicker()
+
+    wu_runner = WaitUntil(0.725, clicker.click_button, *(1, 2, 'ok'), **{'eat': 'shorts', 'qwerty': 12})
+    wu_runner.poll()
+
+    print('WaitUntil gives time elapsed={}'.format(wu_runner.elapsed))
